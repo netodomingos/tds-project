f32d86465c0111e0fa533f367eba2fd98971f833
geraldo_domingos <geraldoneto@locus.software>
2023-01-29 18:50:40 -0300

    Tests deploy

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -68,6 +68,7 @@ export default function Home() {
             ))
           }
         </RowContent>
+        <Title>{import.meta.env.VITE_TEST_ENV}</Title>
       </ViewContent>
     </Container>
   )
